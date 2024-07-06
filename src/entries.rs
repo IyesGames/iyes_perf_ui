ad04c54a5dda06056d67b9c41dd76e14723f63d0
Ida Iyes <identry@protonmail.com>
2024-07-06 19:35:39 +0300

    Add more helper bundles for entries, besides PerfUiCompleteBundle

--- src/entries.rs
+++ src/entries.rs
@@ -98,3 +98,126 @@ pub struct PerfUiCompleteBundle {
     pub window_mode: PerfUiEntryWindowMode,
     pub window_present_mode: PerfUiEntryWindowPresentMode,
 }
+
+/// Bundle for a Perf UI with some of the entry types provided by `iyes_perf_ui`.
+///
+/// This gives you a simple one-liner to spawn a Perf UI!
+///
+/// It will contain an opinionated curated selection of what I consider to
+/// be the most useful of the entries provided by this crate.
+///
+/// Also see [`PerfUiCompleteBundle`].
+///
+/// ```rust
+/// commands.spawn(PerfUiBundle::default());
+/// ```
+///
+/// If you want to create a Perf UI with specific entries of your choice,
+/// just spawn an entity with [`PerfUiRoot`] + your desired entries, instead
+/// of using this bundle.
+///
+/// ```rust
+/// commands.spawn((
+///     PerfUiRoot::default(),
+///     PerfUiEntryFPS::default(),
+///     PerfUiEntryClock::default(),
+///     // ...
+/// ));
+/// ```
+#[allow(missing_docs)]
+#[derive(Bundle, Default)]
+pub struct PerfUiBundle {
+    pub root: PerfUiRoot,
+    pub fps: PerfUiEntryFPS,
+    pub fps_worst: PerfUiEntryFPSWorst,
+    pub frametime: PerfUiEntryFrameTime,
+    pub frametime_worst: PerfUiEntryFrameTimeWorst,
+    pub entity_count: PerfUiEntryEntityCount,
+    pub cursor_position: PerfUiEntryCursorPosition,
+    pub window_resolution: PerfUiEntryWindowResolution,
+}
+
+/// Entries related to framerate.
+///
+/// This bundle exists to help you when spawning a Perf Ui with a custom
+/// selection of entries, if you want to include everything related to
+/// framerate, without listing them one by one.
+///
+/// ```rust
+/// commands.spawn((
+///     PerfUiRoot::default(),
+///     PerfUiFramerateEntries::default(),
+///     // ...
+/// ));
+/// ```
+#[allow(missing_docs)]
+#[derive(Bundle, Default)]
+pub struct PerfUiFramerateEntries {
+    pub fps: PerfUiEntryFPS,
+    pub fps_worst: PerfUiEntryFPSWorst,
+    pub frametime: PerfUiEntryFrameTime,
+    pub frametime_worst: PerfUiEntryFrameTimeWorst,
+}
+
+/// Entries related to system diagnostics.
+///
+/// This bundle exists to help you when spawning a Perf Ui with a custom
+/// selection of entries, if you want to include everything related to
+/// system diagnostics, without listing them one by one.
+///
+/// ```rust
+/// commands.spawn((
+///     PerfUiRoot::default(),
+///     PerfUiSystemEntries::default(),
+///     // ...
+/// ));
+/// ```
+#[allow(missing_docs)]
+#[derive(Bundle, Default)]
+pub struct PerfUiSystemEntries {
+    pub cpu_usage: PerfUiEntryCpuUsage,
+    pub mem_usage: PerfUiEntryMemUsage,
+}
+
+/// Entries related to fixed timestep.
+///
+/// This bundle exists to help you when spawning a Perf Ui with a custom
+/// selection of entries, if you want to include everything related to
+/// fixed timestep, without listing them one by one.
+///
+/// ```rust
+/// commands.spawn((
+///     PerfUiRoot::default(),
+///     PerfUiFixedTimeEntries::default(),
+///     // ...
+/// ));
+/// ```
+#[allow(missing_docs)]
+#[derive(Bundle, Default)]
+pub struct PerfUiFixedTimeEntries {
+    pub fixed_timestep: PerfUiEntryFixedTimeStep,
+    pub fixed_overstep: PerfUiEntryFixedOverstep,
+}
+
+/// Entries related to windowing.
+///
+/// This bundle exists to help you when spawning a Perf Ui with a custom
+/// selection of entries, if you want to include everything related to
+/// windowing, without listing them one by one.
+///
+/// ```rust
+/// commands.spawn((
+///     PerfUiRoot::default(),
+///     PerfUiWindowEntries::default(),
+///     // ...
+/// ));
+/// ```
+#[allow(missing_docs)]
+#[derive(Bundle, Default)]
+pub struct PerfUiWindowEntries {
+    pub cursor_position: PerfUiEntryCursorPosition,
+    pub window_resolution: PerfUiEntryWindowResolution,
+    pub window_scale_factor: PerfUiEntryWindowScaleFactor,
+    pub window_mode: PerfUiEntryWindowMode,
+    pub window_present_mode: PerfUiEntryWindowPresentMode,
+}
